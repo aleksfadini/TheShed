9f15c26b76570011712960b0243b516bef7cab52
aleksfadini <aleksfadini@gmail.com>
2022-03-11 12:18:18 -0500

    fixed extra hearts by mistake

--- Globals.gd
+++ Globals.gd
@@ -1,15 +1,15 @@
 extends Node
 
-var score = 0
-var timer_tot = 0
-const max_hearts=10
-var hearts_halfs=20
-var next_stage=1
+var score:= 0
+var timer_tot:= 0
+const max_hearts:=10
+var hearts_halfs:=20
+var next_stage:=1
 
 func _ready():
 	reset_score()
 
 func reset_score():
 	score = 0
-	timer_tot = 0
-	hearts_halfs = max_hearts*28
+	timer_tot= 0
+	hearts_halfs = max_hearts*2
